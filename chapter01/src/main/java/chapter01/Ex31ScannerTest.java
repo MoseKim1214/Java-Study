f289bbc9c71e2fd296a9d360cada34e64e6541ea
Mose Kim <100126792+MoseKim1214@users.noreply.github.com>
2023-06-26 15:46:02 +0900

    scanner test

---
+++ chapter01/src/main/java/chapter01/Ex31ScannerTest.java
@@ -0,0 +1,27 @@
+package chapter01;
+
+import java.util.Scanner;
+
+public class Ex31ScannerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);//키보드와 연결된 인풋 스트림, ctrl+shift+o 로 임포트해줘야한다.
+		
+		System.out.println("상품 정보를 입력하세요");
+		
+		System.out.print("이름: ");
+		
+		String name = scanner.next(); //여기서 정지됨. block되서 입력을 기다림
+		
+		System.out.println("가격:");
+		int price = scanner.nextInt();
+		
+		System.out.println("재고량");
+		int countStock = scanner.nextInt();
+		
+		System.out.println(name+price + countStock);
+		
+	}
+
+}
