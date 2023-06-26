86f2a69e06effec43a1483626cc3b7581d83fac4
Mose Kim <100126792+MoseKim1214@users.noreply.github.com>
2023-06-26 15:32:43 +0900

    클래스 생성

---
+++ chapter01/src/main/java/chapter02/GoodsApp.java
@@ -0,0 +1,16 @@
+package chapter02;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Goods camera = new Goods();
+		camera.name = "nikon";
+		camera.price = 40000;
+		camera.countStock = 20;
+		camera.countSold = 40;
+		
+		
+	}
+
+}
