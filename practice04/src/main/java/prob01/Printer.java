31c5ae172cbf26887b4895b0d84a5eef6d6c0e49
Mose Kim <100126792+MoseKim1214@users.noreply.github.com>
2023-06-28 17:43:25 +0900

    practice 4

---
+++ practice04/src/main/java/prob01/Printer.java
@@ -0,0 +1,17 @@
+package prob01;
+
+public class Printer {
+	public void println(double num) {
+		System.out.println(num);
+	}
+	public void println(int num) {
+		System.out.println(num);
+	}
+	public void println(String str) {
+		System.out.println(str);
+	}
+	public void println(boolean tf) {
+		System.out.println(tf);
+	}
+	
+}
