90a7a8a3bb4914725006ee2bf99382db5838a548
Mose Kim <100126792+MoseKim1214@users.noreply.github.com>
2023-06-29 10:43:15 +0900

    수정

--- practice01/src/main/java/prob1/Prob1.java
+++ practice01/src/main/java/prob1/Prob1.java
@@ -6,8 +6,9 @@ public class Prob1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
 		
-		int num = scanner.nextInt();
+		
 		System.out.print("수를 입력하시오 : ");
+		int num = scanner.nextInt();
 		if (num%3 == 0) {
 			System.out.println("3의 배수입니다");
 		} else {
