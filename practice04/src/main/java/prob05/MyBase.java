d9d0afce5ab99f3b7eaff3ab63253b93fc8b2eb5
Mose Kim <100126792+MoseKim1214@users.noreply.github.com>
2023-06-29 16:09:35 +0900

    practice4 - prob 5

--- practice04/src/main/java/prob05/MyBase.java
+++ practice04/src/main/java/prob05/MyBase.java
@@ -1,5 +1,36 @@
 package prob05;
 
 public class MyBase extends Base {
+
+	
+	
+	@Override
+	public void service(String state) {
+		
+		
+		if (state.equals("낮")) {
+			day();
+		}else if (state.equals("밤")) {
+			night();
+		
+		}else if (state.equals("오후")) {
+			afternoon();
+		}
+	}
+
+	public void afternoon() {
+		System.out.println("오후도 낮과 마찬가지로 일해야 합니다.");
+		
+	}
+
+	@Override
+	public void day() {
+		
+		System.out.println("낮에는 열심히 일하자!");
+	}
+
+	
 	//다른 파일 건들지 말고 이것만 수정해서 결과 나오도록
+	
+	
 }
